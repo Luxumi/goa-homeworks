356c382a3a75ce8a311fea96b2fe77578bca3cbc
lukhumi <lukhumilukhumi08@gmail.com>
2024-05-12 19:00:04 +0400

    commit

---
+++ Day 26/homework/lesson26.py
@@ -0,0 +1,22 @@
+question = input("სწავლობ გოაში(კი ან არა)?:  ")
+answer = "კი"
+answer1 = "არა"
+answer02 = "13"
+
+
+if question == answer:
+    question1 = input("რომელ ჯგუფში ხარ?:  ")
+    if question1 == answer02:
+        question02 = input("ბატონი გაბრიელი გასწავლის?:   ")
+
+    if question02 == answer:
+        print("სწორია! მეც მასწავლის ბატონი გაბრიელი <3")
+
+    if question02 == answer1:
+        print("ნუ ტყუი!")
+        question3 = input("რომელ ჯგუფში ხარ?")
+        print("ესე არ ჯობია? ჩადები არ იტყუებიან!<3")
+elif question == answer1:
+    print("მაშინ შემოუერთდი გოას")
+else:
+     print("კარგია")
